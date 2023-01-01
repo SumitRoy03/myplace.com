0e5e0f0dafed6f232abb971517dc888a3c46eb16
SumitRoy03 <74671143+SumitRoy03@users.noreply.github.com>
2023-01-01 19:11:43 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,5 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import { axiosInstance } from "../api";
 
 const useFetch = (url) => {
   const [data, setData] = useState([]);
@@ -9,7 +9,7 @@ const useFetch = (url) => {
     const fetchData = async() => {
       setLoading(true);
       try {
-        const res = await axios.get(url);
+        const res = await axiosInstance.get(url);
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -22,7 +22,7 @@ const useFetch = (url) => {
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(url);
+      const res = await axiosInstance.get(url);
       setData(res.data);
     } catch (err) {
       setError(err);
